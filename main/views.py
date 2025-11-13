4a0503b2aaa4e6f5b1a1544bdacd131890076cc9
roychoi1012 <roychoi1012@naver.com>
2025-11-13 17:41:13 +0900

    mysite.conf update

--- main/views.py
+++ main/views.py
@@ -15,8 +15,19 @@ def index(request):
 
 @require_POST
 def add_click(request):
-    click_obj, _created = ClickCount.objects.get_or_create(id=1)
-    click_obj.clicks += 1
-    click_obj.save()
-
-    return JsonResponse({"success": True, "new_count": click_obj.clicks})
+    # POST 요청만 처리합니다.
+    if request.method == 'POST':
+        click_obj = ClickCount.objects.get(id=1)
+        # 카운트 1 증가
+        click_obj.clicks += 1
+        # 데이터베이스에 저장
+        click_obj.save()
+        
+        # 저장 후, 새로 업데이트된 카운트 값을 JSON 형태로 반환합니다. <-- 수정된 부분
+        return JsonResponse({
+            'success': True,
+            'new_count': click_obj.clicks
+        })
+        
+    # POST 요청이 아니면 메인 페이지로 돌려보냅니다.
+    return redirect('main:index')
